f2f49f0d5063c94d947a9664a47dd8461c106fda
baivanco <45272390+baivanco@users.noreply.github.com>
2020-10-10 08:14:22 +0200

    Added

---
+++ main.js
@@ -0,0 +1,28 @@
+//Submit Name to LocalStorage
+function handleSubmit() {
+  const name = document.getElementById("name").value;
+  localStorage.setItem("NAME", name);
+  return;
+}
+
+//Input form Validation
+function validateForm() {
+  var name = document.getElementById("name").value;
+
+  //Minimum 3 characters Validation
+  if (name.length < 3) {
+    document.getElementById("min-char").style.display = "block";
+    return false;
+  } else {
+    document.getElementById("min-char").style.display = "none";
+  }
+
+  //First character Uppercase Validation
+  if (name[0] == name[0].toLowerCase()) {
+    document.getElementById("first-upper").style.display = "block";
+    return false;
+  } else {
+    document.getElementById("first-upper").style.display = "none";
+  }
+  return;
+}
